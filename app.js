5b02491a8c026ef67327daf4c550c2a3ffd2a373
Ravi Shankar <67696330+ravis-web@users.noreply.github.com>
2020-07-29 17:44:52 +0530

    core-types, strict-configs
    - type inference and casting
    - tsconfig and strict-mode
    - union, array type-defintion

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
+"use strict";
+// type-cast
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+// inferred type (always button) and !null
+var btn = document.querySelector("button");
+// arrays
+var numArr = [];
+var strArr = [];
+// union-types
 function add(a, b) {
-    return a + b;
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return a + " " + b; // coercion
+    }
+    else {
+        return +a + +b; // force-conv
+    }
 }
-console.log(add(1, 6));
-console.log(add("1", "6"));
+function printRes(resObj) {
+    console.log(resObj.val, resObj.time);
+}
+/* --- strict-mode ---
+if (btn) {
+  btn.addEventListener("click", () => {
+    const result = add(+a.value, +b.value); // force-conversion
+    console.log(result);
+  });
+}
+*/
+btn.addEventListener("click", function () {
+    var result = add(+a.value, +b.value); // force-conversion
+    numArr.push(result); // type-cast
+    var resStr = add(a.value, b.value);
+    strArr.push(resStr); // type-cast
+    printRes({ val: result, time: new Date() });
+    console.log(numArr, strArr);
+});
--- app.ts
+++ app.ts
@@ -1,7 +1,43 @@
-function add(a, b) {
-  return a + b;
+// type-cast
+const a = document.getElementById("a") as HTMLInputElement;
+const b = document.getElementById("b") as HTMLInputElement;
+
+// inferred type (always button) and !null
+const btn = document.querySelector("button")!;
+
+// arrays
+const numArr: number[] = [];
+const strArr: string[] = [];
+
+// union-types
+function add(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else if (typeof a === "string" && typeof b === "string") {
+    return a + " " + b; // coercion
+  } else {
+    return +a + +b; // force-conv
+  }
 }
 
-console.log(add(1, 6));
+function printRes(resObj: { val: number; time: Date }) {
+  console.log(resObj.val, resObj.time);
+}
 
-console.log(add("1", "6"));
+/* --- strict-mode ---
+if (btn) {
+  btn.addEventListener("click", () => {
+    const result = add(+a.value, +b.value); // force-conversion
+    console.log(result);
+  });
+}
+*/
+
+btn.addEventListener("click", () => {
+  const result = add(+a.value, +b.value); // force-conversion
+  numArr.push(result as number); // type-cast
+  const resStr = add(a.value, b.value);
+  strArr.push(resStr as string); // type-cast
+  printRes({ val: result as number, time: new Date() });
+  console.log(numArr, strArr);
+});
