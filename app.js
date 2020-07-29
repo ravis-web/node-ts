6d2c4681adf2010e550799011990a017e1001065
Ravi Shankar <67696330+ravis-web@users.noreply.github.com>
2020-07-29 20:04:27 +0530

    alias, interface, generics
    - type-aliases and interface
    - generics as array and promise

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 "use strict";
 // type-cast
-var a = document.getElementById("a");
-var b = document.getElementById("b");
+const a = document.getElementById("a");
+const b = document.getElementById("b");
 // inferred type (always button) and !null
-var btn = document.querySelector("button");
+const btn = document.querySelector("button");
 // arrays
-var numArr = [];
-var strArr = [];
+const numArr = [];
+const strArr = [];
+// generics
+const numGenArr = [];
+const strGenArr = [];
 // union-types
+// function add(a: number | string, b: number | string) {
 function add(a, b) {
     if (typeof a === "number" && typeof b === "number") {
         return a + b;
@@ -19,6 +23,9 @@ function add(a, b) {
         return +a + +b; // force-conv
     }
 }
+// using alias
+// function printRes(resObj: { val: number; time: Date }) {
+// function printRes(resObj: Result) {
 function printRes(resObj) {
     console.log(resObj.val, resObj.time);
 }
@@ -30,11 +37,22 @@ if (btn) {
   });
 }
 */
-btn.addEventListener("click", function () {
-    var result = add(+a.value, +b.value); // force-conversion
+btn.addEventListener("click", () => {
+    const result = add(+a.value, +b.value); // force-conversion
     numArr.push(result); // type-cast
-    var resStr = add(a.value, b.value);
+    const resStr = add(a.value, b.value);
     strArr.push(resStr); // type-cast
     printRes({ val: result, time: new Date() });
     console.log(numArr, strArr);
 });
+// Promises : Generics
+const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("promise-resolved");
+    }, 1500);
+});
+prom
+    .then((resp) => {
+    console.log(resp.split("-")); // string-oper
+})
+    .catch();
--- app.ts
+++ app.ts
@@ -9,8 +9,23 @@ const btn = document.querySelector("button")!;
 const numArr: number[] = [];
 const strArr: string[] = [];
 
+// generics
+const numGenArr: Array<number> = [];
+const strGenArr: Array<string> = [];
+
+// aliases
+type NumStr = number | string;
+type Result = { val: number; time: Date };
+
+// interface
+interface ResObj {
+  val: number;
+  time: Date;
+}
+
 // union-types
-function add(a: number | string, b: number | string) {
+// function add(a: number | string, b: number | string) {
+function add(a: NumStr, b: NumStr) {
   if (typeof a === "number" && typeof b === "number") {
     return a + b;
   } else if (typeof a === "string" && typeof b === "string") {
@@ -20,7 +35,10 @@ function add(a: number | string, b: number | string) {
   }
 }
 
-function printRes(resObj: { val: number; time: Date }) {
+// using alias
+// function printRes(resObj: { val: number; time: Date }) {
+// function printRes(resObj: Result) {
+function printRes(resObj: ResObj) {
   console.log(resObj.val, resObj.time);
 }
 
@@ -41,3 +59,16 @@ btn.addEventListener("click", () => {
   printRes({ val: result as number, time: new Date() });
   console.log(numArr, strArr);
 });
+
+// Promises : Generics
+const prom = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise-resolved");
+  }, 1500);
+});
+
+prom
+  .then((resp) => {
+    console.log(resp.split("-")); // string-oper
+  })
+  .catch();
